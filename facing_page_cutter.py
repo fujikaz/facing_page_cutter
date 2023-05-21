46a84ff9dd7def0ba897fc807e5cc9bffeb71d82
FUJIHARA Kazuhiro <fujikaz@cloudnine.jp>
2023-05-21 14:18:57 +0900

    autodetection of cutting direction

--- facing_page_cutter.py
+++ facing_page_cutter.py
@@ -8,11 +8,12 @@ import re
 import copy
 
 ROTATE_ANGLE = 90
+vcut = False
 
 # parse options and args
 usage = "usage: /path/to/%prog [options] <pdf filename w/ .pdf>"
 parser = OptionParser(usage = usage)
-parser.add_option("-v", "--vertical", action="store_true", dest="vertical", help="縦書き")
+parser.add_option("-v", "--vertical", action="store_true", dest="vertical", help="縦書き(右開き)")
 
 (options, args) = parser.parse_args()
 
@@ -31,20 +32,28 @@ page = pdf_reader.getPage(0)
 (x0, y0) = page.mediaBox.getLowerLeft()
 (x1, y1) = page.mediaBox.getUpperRight()
 
-# crop half size pages and rotate
+if x1 - x0 > y1 - y0:
+    vcut = True
+
+# crop half size pages and rotate if needed
 for i in range(pdf_reader.getNumPages()):
     p1 = pdf_reader.getPage(i)
     p2 = copy.copy(p1)
-    p1.cropBox.lowerLeft = (x0, y0)
-    p1.cropBox.upperRight = (x1, (y0 + y1) / 2)
-    p2.cropBox.lowerLeft = (x0, (y0 + y1) / 2)
-    p2.cropBox.upperRight = (x1, y1)
+    if vcut == True:
+        p1.cropBox.lowerLeft = (x0, y0)
+        p1.cropBox.upperRight = ((x0 + x1) / 2, y1)
+        p2.cropBox.lowerLeft = ((x0 + x1) / 2, y0)
+        p2.cropBox.upperRight = (x1, y1)
+    else:
+        p1.cropBox.lowerLeft = (x0, y0)
+        p1.cropBox.upperRight = (x1, (y0 + y1) / 2)
+        p2.cropBox.lowerLeft = (x0, (y0 + y1) / 2)
+        p2.cropBox.upperRight = (x1, y1)
+        p1.rotateClockwise(ROTATE_ANGLE)
+        p2.rotateClockwise(ROTATE_ANGLE)
     
     if options.vertical is True:
         p1, p2 = p2, p1
-    
-    p1.rotateClockwise(ROTATE_ANGLE)
-    p2.rotateClockwise(ROTATE_ANGLE)
 
     pdf_writer.addPage(p1)
     pdf_writer.addPage(p2)
